91bddb5366a68b0cfdaf78ffb636f5dcc70b01e1
kurniagung <agungkurniadi99@gmail.com>
2023-11-20 18:01:22 +0700

    add fetch helper for about slice

--- lib/fetch-prismic.ts
+++ lib/fetch-prismic.ts
@@ -2,7 +2,14 @@ import { createClient } from '@/prismicio';
 
 export async function getMetadata() {
   const client = createClient();
-  const data = await client.getSingle('metadata');
+  const page = await client.getSingle('metadata');
 
-  return data.data;
+  return page.data;
+}
+
+export async function getAbout() {
+  const client = createClient();
+  const page = await client.getSingle('about');
+
+  return page.data;
 }
