c7f4921ed4fa91c4332d748301d8281459d66fd8
kurniagung <agungkurniadi99@gmail.com>
2023-11-20 18:00:10 +0700

    add remote pattern imaage for prismic image

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
-const path = require("path");
+const path = require('path');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    remotePatterns: [{ hostname: 'images.prismic.io' }],
+  },
   sassOptions: {
-    includePaths: [path.join(__dirname, "styles")],
+    includePaths: [path.join(__dirname, 'styles')],
   },
 };
 
