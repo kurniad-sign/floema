85994b3f85f5ff1248255ef54a4670a73b4250f6
kurniagung <agungkurniadi99@gmail.com>
2023-11-21 08:58:37 +0700

    add basic home page data

--- app/(route)/page.tsx
+++ app/(route)/page.tsx
@@ -1,10 +1,46 @@
-import { createClient } from '@/prismicio';
+import Link from 'next/link';
+import { PrismicNextImage } from '@prismicio/next';
+
+import { getHome } from '@/lib/fetch-prismic';
 
 export default async function Home() {
-  const client = createClient();
-  const page = await client.getSingle('metadata');
+  const { collections, home } = await getHome();
 
-  console.log(page.data);
+  return (
+    <div className="home">
+      <div className="home__wrapper">
+        <div className="home__titles">
+          {collections.map((collection, index) => (
+            <>
+              <div className="home__titles__label">
+                {home.collections}{' '}
+                {index === 0
+                  ? 'One'
+                  : index === 1
+                    ? 'Two'
+                    : index === 2
+                      ? 'Three'
+                      : index === 3
+                        ? 'Four'
+                        : ''}
+              </div>
+              <div className="home__titles__title">{collection.title}</div>
+            </>
+          ))}
+        </div>
+        <div className="home__gallery">
+          {home.gallery.map((gallery) => (
+            <figure key={gallery.image.id} className="home__gallery__media">
+              <PrismicNextImage
+                field={gallery.image}
+                className="home__gallery__media__image"
+              />
+            </figure>
+          ))}
+        </div>
 
-  return <main>Hello World</main>;
+        <Link href={'/collections'}>{home.button}</Link>
+      </div>
+    </div>
+  );
 }
